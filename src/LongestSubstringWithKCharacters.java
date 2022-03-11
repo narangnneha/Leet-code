f1eae0d35316336e5247301d0cfa47bcbcae5610
Neha Narang <neha.narang@apple.com>
2022-03-10 17:05:47 -0800

    leet code solution

---
+++ src/LongestSubstringWithKCharacters.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithKCharacters {
+
+    public static String findLongestSubstring(String str, int k)
+    {
+        // base case
+        if (str == null || str.length() == 0) {
+            return str;
+        }
+        Map<Character,Integer> map = new HashMap<>();
+
+        int end = 0; int begin  =0 ;
+
+        for (int low = 0, high  = 0 ;  high < str.length() ; high ++) {
+            map.put(str.charAt(high), map.getOrDefault(str.charAt(high), 0) + 1);
+
+            while (map.size() > k){
+                map.put(str.charAt(low), map.get(str.charAt(low)) - 1 );
+                if (map.get(str.charAt(low)) == 0) {
+                    map.remove(str.charAt(low));
+                }
+                low++;
+
+            }
+            if (end - begin < high - low) {
+                end = high;
+                begin = low;
+            }
+        }
+        return str.substring(begin, end + 1);
+
+        }
+
+    public static void main(String[] args)
+    {
+        String str = "abcbdbdbbdcdabd";
+        int k = 3;
+
+        System.out.print(findLongestSubstring(str, k));
+    }
+}
