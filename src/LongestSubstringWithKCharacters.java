d0cac080a88d0a43cb5fdbf70049b9e546cc8475
Neha Narang <neha.narang@apple.com>
2022-03-10 17:10:22 -0800

    leet code solution

--- src/LongestSubstringWithKCharacters.java
+++ src/LongestSubstringWithKCharacters.java
@@ -9,6 +9,7 @@ public class LongestSubstringWithKCharacters {
         if (str == null || str.length() == 0) {
             return str;
         }
+        //Map to hold the frequency of each characters
         Map<Character,Integer> map = new HashMap<>();
 
         int end = 0; int begin  =0 ;
@@ -16,6 +17,7 @@ public class LongestSubstringWithKCharacters {
         for (int low = 0, high  = 0 ;  high < str.length() ; high ++) {
             map.put(str.charAt(high), map.getOrDefault(str.charAt(high), 0) + 1);
 
+            //if the size of the window have exceeded  check for the left most character, if the frequency becomes zero it should also be removed from map
             while (map.size() > k){
                 map.put(str.charAt(low), map.get(str.charAt(low)) - 1 );
                 if (map.get(str.charAt(low)) == 0) {
